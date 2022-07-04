443ba0756497077a3f72ac144b7ea29d42dce411
goutham <gouthamshine@gmail.com>
2022-07-04 23:02:18 +0530

    fixed size vertical grid

---
+++ GridSystem/VerticalGrid.swift
@@ -0,0 +1,40 @@
+//
+//  VerticalGrid.swift
+//  GridSystem
+//
+//  Created by Goutham S on 04/07/22.
+//
+
+import SwiftUI
+
+struct VerticalGrid: View {
+    let items = (0...200)
+    let columns = [
+        GridItem(.fixed(100)),
+        GridItem(.fixed(100)),
+        GridItem(.fixed(100)),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, content: {
+                ForEach(items, id: \.self) { item in
+                    ZStack {
+                        Circle()
+                            .strokeBorder(Color.blue, lineWidth: 4)
+                        Text("\(item)")
+                            .font(.title3)
+                            .padding()
+                    }
+                    .background(Color.yellow)
+                }
+            })
+        }
+    }
+}
+
+struct VerticalGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalGrid()
+    }
+}
---
+++ GridSystem/VerticalView.swift
@@ -0,0 +1,20 @@
+//
+//  VerticalView.swift
+//  GridSystem
+//
+//  Created by Goutham S on 04/07/22.
+//
+
+import SwiftUI
+
+struct VerticalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VerticalView_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalView()
+    }
+}
