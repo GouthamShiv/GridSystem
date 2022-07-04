7509dfb23c11ccdd8922af7b05a7141bba69ca99
goutham <gouthamshine@gmail.com>
2022-07-04 22:37:43 +0530

    Initial Commit

---
+++ GridSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridSystem
+//
+//  Created by Goutham S on 04/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridSystem/GridSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSystemApp.swift
+//  GridSystem
+//
+//  Created by Goutham S on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GridSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
